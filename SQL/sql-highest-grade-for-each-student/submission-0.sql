1bb0ca0c3f338fb4ca4c8f5b55441fa694a3f07e
DEEPASHREE SRINIVASA RAO RANNORE <91663918+DeepashreeSRannore@users.noreply.github.com>
2026-04-04 20:20:16 -0400

    Add: sql-highest-grade-for-each-student - submission-0

---
+++ SQL/sql-highest-grade-for-each-student/submission-0.sql
@@ -0,0 +1,11 @@
+-- Write your query below
+select student_id, exam_id, score
+from ( 
+    select student_id, exam_id, score,
+    rank() over(
+        partition by student_id
+        order by score DESC, exam_id asc) as rn
+    from exam_results
+)ranked
+where rn = 1
+order by student_id;

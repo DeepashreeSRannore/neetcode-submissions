24fbfc0f931e73396e7551f813f9ebb4473ac207
DEEPASHREE SRINIVASA RAO RANNORE <91663918+DeepashreeSRannore@users.noreply.github.com>
2026-04-06 20:54:30 -0400

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = " ".join(a for a in s if a.isalnum())
+        lp = 0
+        rp = len(s)-1
+        
+        
+        while lp < rp:
+            if s[lp].lower() != s[rp].lower():
+                return False
+            lp+=1
+            rp-=1
+        
+        else:
+            return True
+
+        #TC=O(n)
+        #SC=O(1)
+        
+
+        
